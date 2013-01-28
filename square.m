34959fd852003d2f879af85ec2979c7c35bf9277
Haukur O. Th <hawkuro@gmail.com>
2013-01-28 12:23:27 +0000

    Kláraði square.m

--- square.m
+++ square.m
@@ -5,16 +5,19 @@ function P = square(a,b,c,d)
     hold on
     hnappur = 1;
     i = 1;
-    P = [ 0 0 0 0 0; 0 0 0 0 0];
-    while hnappur == 1;
+    P = [ 0 0 0 0 0; 0 0 0 0 0]; %=zeros(2,5)
+    while hnappur == 1 && i <= 4;
             [x,y,hnappur] = ginput(1);
                 if hnappur == 1 && i <= 4;
                 P(1,i) = x;
                 P(2,i) = y;
                 plot(x,y,'o');
                 i = i+1;
-            end
+                end
+    end
     P(1,5) = P(1,1);
     P(2,5) = P(2,1);
+    
+    plot(P(1,1:5),P(2,1:5)) %teiknum kassann
 end
             
