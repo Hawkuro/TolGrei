647c755200099c3f64b9204888aff20c35697c81
bjarki <bjarki@BJ-School.(none)>
2013-01-29 17:54:10 +0000

    Bætti við skránum frá ragnari

---
+++ innlestur.m
@@ -0,0 +1,15 @@
+% Matlab-forrit sem les hnit punkts með músarsmelli á 
+%vinstri músartakka  innan gefins ramma og teiknar hring 
+% um punktinn.  Forritið stöðvar er ýtt er á hægri músartakka.
+%
+a=-1; b=1; c=-1; d=1;      % Mörkin sett á breyturnar
+clf                        % Mynd hreinsuð.
+axis([a b c d])            % Mynd sett upp.
+hold on
+hnappur=1;
+while hnappur==1           
+  [x,y,hnappur]=ginput(1);
+  if hnappur==1
+    plot(x,y,'o')
+  end
+end
