572fa330093ed61e551c7229bd9410642b4e7db6
Hawkuro <hawkuro@gmail.com>
2013-02-05 16:18:28 +0000

    Smá hreingerning áður en ég byrja að forrita lið 4

--- newton_gradient.m
+++ newton_gradient.m
@@ -1,31 +1,37 @@
-function x = newton_gradient(f,epsilon, delta, nmax, x0, P);
+function x = newton_gradient(f,epsilon, delta, nmax, x0, P, axis)
    %Skgr. follin sem vid notum
-   a =0, b = 1, c = 0, d = 1;
+   a =0; b = 1; c = 0; d = 1;
+   if(nargin == 7)
+       a = axis(1);
+       b = axis(2);
+       c = axis(3);
+       d = axis(4);
+   end
+   
    h1 = 0.01 * min(b-a,d-c);
-  % h1 = 0.01
    F = @(x) [ (f(x(1) + h1, x(2)) - f(x(1)-h1,x(2)))/(2*h1); (f(x(1), x(2) + h1) - f(x(1),x(2)-h1))/(2*h1)];
    A = @(x) (f(x(1) + h1, x(2)) + f(x(1)-h1,x(2)) - 2*f(x(1),x(2)))/(h1*h1);
-   B = @(x) ( f(x(1) + h1, x(2)+h1) - f(x(1) + h1, x(2)-h1) - f(x(1) - h1, x(2)+h1) + f(x(1) - h1, x(2) -h1))/(4*h1*h1)
+   B = @(x) ( f(x(1) + h1, x(2)+h1) - f(x(1) + h1, x(2)-h1) - f(x(1) - h1, x(2)+h1) + f(x(1) - h1, x(2) -h1))/(4*h1*h1);
    C = @(x) (f(x(1) , x(2)+ h1) + f(x(1),x(2)-h1) - 2*f(x(1),x(2)))/(h1*h1);
-   dF = @(x) [ A(x) B(x); B(x) C(x)]
+   dF = @(x) [ A(x) B(x); B(x) C(x)];
      
     % Upphafsstilling:
     n=0;
     x=x0; 
     %fprintf('%1d  %21.15e\n',n,x')
-    y=F(x)
-    dy=dF(x) 
+    y=F(x);
+    dy=dF(x);
     h=-dy\y;
-    x=x+h 
+    x=x+h;
     n=1; 
     e0=norm(h);
     e=2*epsilon;
-    while e>epsilon & norm(y)>delta & n<nmax
+    while e>epsilon && norm(y)>delta && n<nmax && square_check(x,P)
         y=F(x); dy=dF(x);
         h=-dy\y; 
         e=norm(h);
-        x=x+h  
-        n=n+1  
+        x=x+h;
+        n=n+1;
         e0=e;
     end
 end
