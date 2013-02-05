46de53cfc2c413a0d97eb8174e5fa63848445d34
Matthias Pall Gissurarson <mpg@mpg.is>
2013-02-05 17:33:00 +0000

    F og dF faert utfyrir, newtont_gradeint breytt i samræmi við það

--- newton_gradient.m
+++ newton_gradient.m
@@ -9,29 +9,25 @@ function x = newton_gradient(f,epsilon, delta, nmax, x0, P, axis)
    end
    
    h1 = 0.01 * min(b-a,d-c);
-   F = @(x) [ (f(x(1) + h1, x(2)) - f(x(1)-h1,x(2)))/(2*h1); (f(x(1), x(2) + h1) - f(x(1),x(2)-h1))/(2*h1)];
-   A = @(x) (f(x(1) + h1, x(2)) + f(x(1)-h1,x(2)) - 2*f(x(1),x(2)))/(h1*h1);
-   B = @(x) ( f(x(1) + h1, x(2)+h1) - f(x(1) + h1, x(2)-h1) - f(x(1) - h1, x(2)+h1) + f(x(1) - h1, x(2) -h1))/(4*h1*h1);
-   C = @(x) (f(x(1) , x(2)+ h1) + f(x(1),x(2)-h1) - 2*f(x(1),x(2)))/(h1*h1);
-   dF = @(x) [ A(x) B(x); B(x) C(x)];
-     
-    % Upphafsstilling:
-    n=0;
-    x=x0; 
-    %fprintf('%1d  %21.15e\n',n,x')
-    y=F(x);
-    dy=dF(x);
-    h=-dy\y;
-    x=x+h;
-    n=1; 
-    e0=norm(h);
-    e=2*epsilon;
-    while e>epsilon && norm(y)>delta && n<nmax && square_check(x,P)
-        y=F(x); dy=dF(x);
-        h=-dy\y; 
-        e=norm(h);
-        x=x+h;
-        n=n+1;
-        e0=e;
-    end
+   F = @(x) F(f,h1,x)
+   dF = @(x) dF(f,h1,x) 
+   % Upphafsstilling:
+   n=0;
+   x=x0; 
+   %fprintf('%1d  %21.15e\n',n,x')
+   y=F(x);
+   dy=dF(x);
+   h=-dy\y;
+   x=x+h;
+   n=1; 
+   e0=norm(h);
+   e=2*epsilon;
+   while e>epsilon && norm(y)>delta && n<nmax && square_check(x,P)
+       y=F(x); dy=dF(x);
+       h=-dy\y; 
+       e=norm(h);
+       x=x+h;
+       n=n+1;
+       e0=e;
+   end
 end
