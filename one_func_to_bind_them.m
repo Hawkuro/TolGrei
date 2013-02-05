80307df0eb5e8678225004266ac96bafbb78755e
Matthias Pall Gissurarson <mpg@mpg.is>
2013-02-05 18:01:21 +0000

    lykkja

--- one_func_to_bind_them.m
+++ one_func_to_bind_them.m
@@ -21,12 +21,23 @@ function one_func_to_bind_them(axiss,epsilon,delta,nmax)
 	contour(X,Y,Z,50)
 
 	hold on
-    
-	[P,x0]=square(a,b,c,d);
-    
-    p = newton_gradient(f,epsilon,delta,nmax,x0,P,axiss);
-    plot(p(1),p(2),'o')
-    
-    h1 = 0.01 * min(b-a,d-c);
-    Hessian = dFgeneral(f,h1,p)
+	hnappur = 1;
+	while hnappur == 1	
+		P = square(a,b,c,d);
+
+		[x, y, hnappur] = ginput(1);
+		plot(x,y,'x');
+		x0 = [ x; y];
+
+		p = newton_gradient(f,epsilon,delta,nmax,x0,P,axiss);
+		if(square_check(p,P)) 
+			plot(p(1),p(2),'o')
+			h1 = 0.01 * min(b-a,d-c);
+			Hessian = dFgeneral(f,h1,p)
+		end
+		%else
+			%clf;
+			%contour(X,Y,Z,50)
+	end
+
 end
--- square.m
+++ square.m
@@ -1,5 +1,5 @@
 %Tekur inn hnit med musasmellum og skilar fylki med theim i rettri rod.
-function [P x0] = square(a,b,c,d)
+function P = square(a,b,c,d)
     %Teiknum hnitakerfi
     axis([a b c d])
     hold on
@@ -7,10 +7,10 @@ function [P x0] = square(a,b,c,d)
     i = 1;
     %Buum til fylki til a√∞ geyma ni√∞urst√∂√∞ur.
     P = [ 0 0 0 0 0; 0 0 0 0 0];
-    while hnappur == 1;
+    while hnappur == 1 && i <= 4;
 	[x,y,hnappur] = ginput(1);
 	%Viljum bara f√° 4 punkta.
-	if hnappur == 1 && i <= 4;
+	if hnappur == 1;
 	    P(1,i) = x;
 	    P(2,i) = y;
 	    plot(x,y,'o');
@@ -21,9 +21,6 @@ function [P x0] = square(a,b,c,d)
     P = P(:,flipdim(convhull(P(1,1:4)',P(2,1:4)'),1));
     test = P(:,5); %TÈkkar hvort convhull gefi fjÛra punkta, og ˛vÌ hvort ferhyrningurinn sÈ k˙ptur.
     plot(P(1,:),P(2,:))
-    [x, y, hnappur] = ginput(1);
-    plot(x,y,'x');
-    x0 = [ x; y];
 end
 
 
