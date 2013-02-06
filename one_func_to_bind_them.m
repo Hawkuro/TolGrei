8055f70906cd62d2670536dd2d6b5094c9192d03
Matthias Pall Gissuarson <mpg@mpg.is>
2013-02-06 12:57:25 +0000

    Bætti við stöðupunkta greiningu, og lét fallið taka inn fall, í staðinn fyrir að skilgreina það í fallinu. Slökkti einnig á útprentunum í greiningunni

--- one_func_to_bind_them.m
+++ one_func_to_bind_them.m
@@ -1,4 +1,4 @@
-function one_func_to_bind_them(axiss,epsilon,delta,nmax)
+function one_func_to_bind_them(f,axiss,epsilon,delta,nmax)
 	a=axiss(1);
 	b=axiss(2);
 	c=axiss(3);
@@ -6,8 +6,6 @@ function one_func_to_bind_them(axiss,epsilon,delta,nmax)
 
 	axis(axiss);
     
-    	f = @(x,y) func(x,y);
-
 	x = linspace(a,b,250);
 	y = linspace(c,d,250);
     
@@ -24,30 +22,53 @@ function one_func_to_bind_them(axiss,epsilon,delta,nmax)
 	hnappur = 1;
 	while hnappur == 1	
 		P = square(a,b,c,d);
-
-		[x, y, hnappur] = ginput(1);
+        
+        [x, y, hnappur] = ginput(1);
 		plot(x,y,'x');
 		x0 = [ x; y];
-
-		p = newton_gradient(f,epsilon,delta,nmax,x0,P,axiss);
-		if(square_check(p,P)) 
-			plot(p(1),p(2),'o')
-			h1 = 0.01 * min(b-a,d-c);
-			Hessian = dFgeneral(f,h1,p);
+        
+        while (square_check(x0,P) ~= 1)
+            [x, y, hnappur] = ginput(1);
+            plot(x,y,'x');
+            x0 = [ x; y];
+        end
+		
+        
+        
+        p = newton_gradient(f,epsilon,delta,nmax,x0,P,axiss);
+        if(square_check(p,P)) 
+            h1 = 0.01 * min(b-a,d-c);
+            Hessian = dFgeneral(f,h1,p);
             break;
-		else
-			clf;
-			contour(X,Y,Z,50)
+        else
+            clf;
+            contour(X,Y,Z,50)
             hold on
-			plot(p(1),p(2),'o')
-			plot(x,y,'x');
+            plot(p(1),p(2),'o')
+            plot(x,y,'x');
             plot(P(1,:),P(2,:),'--')
         end
     end
-    if(det(Hessian) ~= 0)
+    
+    M = det(Hessian);
+    if(M > 0)
         eigs = eig(Hessian);
+        if eigs(1) > 0 && eigs(2) > 0
+            %lagpunktur
+            plot(p(1),p(2),'v')
+        else
+            if eigs(1) < 0 && eigs(2) < 0
+                %hapunktur
+                plot(p(1),p(2),'^')
+            end
+        end
     else
-        %lame
+        if M < 0
+            %Sodulpunktur
+            plot(p(1),p(2),'*')
+        else
+            %Test inconclusive
+            plot(p(1),p(2),'o')
+        end
     end
-
 end
