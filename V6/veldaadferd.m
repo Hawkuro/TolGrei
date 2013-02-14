8f065402c31829328c58262eb9fe18f44ae27392
Matthias Pall Gissuarson <mpg@mpg.is>
2013-02-14 18:14:14 +0000

    Lagadi sma indent.

--- V6/veldaadferd.m
+++ V6/veldaadferd.m
@@ -1,7 +1,7 @@
 function x = veldaadferd()
-A = [3 2 -2; -3 -1 3; 1 2 0];
-x0 = [1;0;0];
-veldaadferdworker(A,x0,5)
+    A = [3 2 -2; -3 -1 3; 1 2 0];
+    x0 = [1;0;0];
+    veldaadferdworker(A,x0,5)
 end
 
 function lambda = veldaadferdworker(A,x,m)
