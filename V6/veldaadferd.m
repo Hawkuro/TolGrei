2c59fb5dd20f59cee82f519e6eebec571b0d8ec2
Matthias Pall Gissuarson <mpg@mpg.is>
2013-02-14 18:13:11 +0000

    Klaradi v1.2.

---
+++ V6/veldaadferd.m
@@ -0,0 +1,18 @@
+function x = veldaadferd()
+A = [3 2 -2; -3 -1 3; 1 2 0];
+x0 = [1;0;0];
+veldaadferdworker(A,x0,5)
+end
+
+function lambda = veldaadferdworker(A,x,m)
+    i = 0;
+    x %Prentum ut x^(0), til ad sja rununa.
+    while i < m
+        y = A*x;
+        %Finnum inf-normid med minnsta indexid.
+        x = y/y(find(abs(y)==norm(y,Inf),1))
+        i = i+1;
+    end
+    %Viljum finna eigingildid lika, til thess er adferdin
+    lambda = y(find(abs(y)==norm(y,Inf),1));
+end
