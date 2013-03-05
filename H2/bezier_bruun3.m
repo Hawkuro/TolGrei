3d70e4424b735458d1f149bf2a97a901f6317f68
Hawkuro <hawkuro@gmail.com>
2013-03-05 22:32:37 +0000

    bezier_bruun3 komin með allt sem beðið er um

--- H2/bezier_bruun3.m
+++ H2/bezier_bruun3.m
@@ -13,6 +13,8 @@
     n=4;
     t=0:1;
     tt=linspace(0,1,100);
+    xxx = []; %Hehe
+    yyy = [];
 
     while hnappur==1
         [xtmp,ytmp,hnappur]=ginput(1);
@@ -29,10 +31,21 @@
             plot([x(2) x(1)], [y(2) y(1)], 'r')
             plot([x(4) x(3)], [y(4) y(3)], 'r')
             xx = baz(x,tt);
+            xxx = [xxx xx]
             yy = baz(y,tt);
+            yyy = [yyy yy]
             plot(xx,yy)
             
             x = [x(4) (2*x(4)-x(3))];
             y = [y(4) (2*y(4)-y(3))];
         end
     end
+    
+        
+    close all
+    a=-1; b=1; c=-1; d=1;
+    axis([a b c d])
+
+    % Lesum inn punkta
+    hold on
+    plot(xxx,yyy)
