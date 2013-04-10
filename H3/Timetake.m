83eaf29c37c4f377f0d338d745debbc34a4b47dd
Hawkuro <hawkuro@gmail.com>
2013-04-10 09:19:21 +0000

    leiðrétting í pdf-verkefni

--- H3/Timetake.m
+++ H3/Timetake.m
@@ -7,10 +7,10 @@ for i=1:numtests
     adams_pc5('Test',0,0,5,100);
     adamstimes(i)=toc;
     tic
-    rkf45('Test',0,0,5,[0.0001,1,1]);
+    rkf45('Test',0,0,5,[0.00001,1,0.00001]);
     rkf45times(i)=toc;
     tic
-    rkv56('Test',0,0,5,[0.0001,1,1]);
+    rkv56('Test',0,0,5,[0.00001,1,0.00001]);
     rkv56times(i)=toc;
 end
 adamsmeantime=sum(adamstimes)/numtests
