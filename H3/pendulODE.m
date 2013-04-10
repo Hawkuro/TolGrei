b21e4d928a7d91084e6f63866f585e3459892cd4
Matthias Pall Gissurarson <mpg@mpg.is>
2013-04-10 12:35:08 +0000

    ode restored

--- H3/pendulODE.m
+++ H3/pendulODE.m
@@ -4,6 +4,6 @@ function y = pendulODE (t,x)
     l = 1;
     omega = sqrt(g/l);
     y(1) = x(2);
-    %y(2) = -(omega^2)*sin(x(1));
+    y(2) = -(omega^2)*sin(x(1));
     % prufa linulega nalgun
-    y(2) = -(omega^2)*x(1);
+    %y(2) = -(omega^2)*x(1);
