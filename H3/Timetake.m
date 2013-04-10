d68d333de41a9249928c4face6ca278a32216dc6
bjarki <bjarkigeirbenediktsson@gmail.com>
2013-04-10 01:58:57 +0000

    var að bæta við tímadótið

--- H3/Timetake.m
+++ H3/Timetake.m
@@ -1,10 +1,18 @@
-tic
-adams_pc5('Test',0,0,5,100);
-at=toc
-tic
-rkf45('Test',0,0,5,[0.0001,1,1]);
-rkf45t=toc
-tic
-rkv56('Test',0,0,5,[0.0001,1,1]);
-rkv56t=toc
-%athugum að þessum lið er ekki lokið sjá verklýsingu
+adamstimes=[];
+rkf45times=[];
+rkv56times=[];
+numtests=100
+for i=1:numtests
+    tic
+    adams_pc5('Test',0,0,5,100);
+    adamstimes(i)=toc;
+    tic
+    rkf45('Test',0,0,5,[0.0001,1,1]);
+    rkf45times(i)=toc;
+    tic
+    rkv56('Test',0,0,5,[0.0001,1,1]);
+    rkv56times(i)=toc;
+end
+adamsmeantime=sum(adamstimes)/numtests
+rkf45meantime=sum(rkf45times)/numtests
+rkv56meantime=sum(rkv56times)/numtests
