16c72cb8ea6d0affca5978986573dbec63d5892e
Matthias Pall Gissurarson <mpg@mpg.is>
2013-04-10 12:24:20 +0000

    vinna ad lid 2

--- H3/pendulODE.m
+++ H3/pendulODE.m
@@ -1,5 +1,9 @@
 function y = pendulODE (t,x)
-    g = 9.82;
+    %g = 9.82;
+    g = 1;
     l = 1;
+    omega = sqrt(g/l);
     y(1) = x(2);
-    y(2) = -(g/l)*cos(x(1));
+    %y(2) = -(omega^2)*sin(x(1));
+    % prufa linulega nalgun
+    y(2) = -(omega^2)*x(1);
--- H3/pendull.m
+++ H3/pendull.m
@@ -10,34 +10,51 @@ fig=figure;
 %% Fastar
 lotur = 5;          %Fjoldi lota til að reikna
 n = 25;             %Fjoldi mynda i hverri lotu               
-theta0 = 1;         %theta(t0)
+theta0 = 0.3;         %theta(t0)
 theta1 = 0;         %theta'(t0)
 omega = 1;          %Hornhradi
 t0 = 0;
 %% Jofnurnar
 theta = @(t) theta0*cos(omega*(t-t0)) + (theta1/omega) * sin(omega*(t-t0));
-dtheta = @(t) -omega*theta0*sin(omega*(t-t0))+ theta1*cos(omega*(t-t0));
+dtheta = @(t) -omega*theta0*sin(omega*(t-t0))+ theta1*cos(omega*(t- ...
+                                                  t0));
+
+simple = adams_pc5('pendulODE',t0,[theta0,theta1],2*pi*lotur,lotur*n);
+thetasimple = @(t)  simple(1, floor(t*n/(2*pi)) + 1);
+dthetasimple = @(t) simple(2, floor(t*n/(2*pi)) + 1);
 %%
 % Thar sem við hreinsum myndina i hverju skrefi ta thurfum vid ad geyma
 % fasahnitin i fylki
 fasahnit = [theta(0);dtheta(0)];
+simplefasahnit =[thetasimple(0);dthetasimple(0)];
 for t = 0:2*pi/n:2*pi*lotur
-    %% Pendullinn    
+    %% Pendullinn
     subplot(2,1,1) %Skiptir myndaflotinum i 2x1 fylki og segir matlab ad nota fyrsta stakid
     % Teikniskipun fyrir pendulinn: teiknum linu fra [0,0] (festipunktur) 
     % i [sin(theta(t)),-cos(theta(t))] sem er stadsetning lodsins
     % '-o' segir ad vid aetlum ad teikna linu med hringlaga endapunkta
     % 'MarkerSize' setur staerd endapunktanna
     % 'MarkerFaceColor' akvardar lit endapunktana
-    plot([0,sin(theta(t))],[0,-cos(theta(t))],'-o','MarkerSize',8,'MarkerFaceColor','r') 
+    plot([0,sin(theta(t))],[0,-cos(theta(t))],'-o','MarkerSize',8, ...
+         'MarkerFaceColor','r') 
+    hold on
+    plot([0,sin(thetasimple(t))],[0,-cos(thetasimple(t))],'-o','MarkerSize',8,'MarkerFaceColor','b') 
     axis([-1.2,1.2,-1.2,1.2]) %Festir asana
-    axis square %Thvingar matlab til ad hafa x og y asinn jafn langan
+    axis square %Thvingar matlab til ad hafa x og y asinn jafn
+                %langan
+    hold off
     %% Fasaritid
     subplot(2,1,2) %Skipar matlab ad nota seinni hlutan af myndflotinum
-    fasahnit = [fasahnit(1,:) theta(t);fasahnit(2,:) dtheta(t)]; %Baetir nyju fasahnitunum vid thau gomlu.
+    fasahnit = [fasahnit(1,:) theta(t); fasahnit(2,:) dtheta(t)]; %Baetir nyju fasahnitunum vid thau gomlu.
+    simplefasahnit = [simplefasahnit(1,:) thetasimple(t);simplefasahnit(2,:) dthetasimple(t)]; %Baetir nyju fasahnitunum vid thau gomlu.
     plot(fasahnit(1,:),fasahnit(2,:)) % Linan i fasaritinu
+    hold on
+    plot(simplefasahnit(1,:),simplefasahnit(2,:)) % Linan i fasaritinu
     hold on % Thurfum ad setja "hold on" her svo vid yfirskrifum ekki linuna i fasaritnu bara med punktinum
-    plot(theta(t),dtheta(t),'or', 'MarkerSize', 6) %Punkturinn i fasaritinu
+    plot(theta(t),dtheta(t),'ob', 'MarkerSize', 6) %Punkturinn i
+                                                   %fasaritinu
+    hold on 
+    plot(thetasimple(t),dthetasimple(t),'or', 'MarkerSize', 6) %Punkturinn i fasaritinu
     axis([-1.2,1.2,-1.2,1.2])
     axis square
     hold off 
@@ -48,3 +65,5 @@ end
 %% Fragangur
 close(fig); %Lokar myndinni til að ekki se haegt að yfirskrifa hana
 aviobj = close(aviobj); %Lokar og byr til myndbandid
+simplefasahnit
+fasahnit
