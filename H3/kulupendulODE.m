e76a285f23c43d02eaf21e38356528911cbe6a74
Matthias Pall Gissurarson <mpg@mpg.is>
2013-04-11 22:10:20 +0000

    ode guess

---
+++ H3/kulupendulODE.m
@@ -0,0 +1,10 @@
+function y = kulupendulODE(t,x)
+    m = 1;
+    l = 1;
+    g = 1;
+    om = 1/(m*l*l);
+    
+    y(1) = om*x(3);
+    y(2) = om*x(4)/(sin(x(1))^2);
+    y(3) = om*x(4)*(cos(x(1))/(sin(x(1))^3)) - m*l*g*sin(x(1));
+    y(4) = 0;
