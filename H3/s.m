b58b94d60493cac5cd14315445fbad528b0d301c
bjarki <bjarkigeirbenediktsson@gmail.com>
2013-04-12 03:03:45 +0000

    smá lagfæring

--- H3/s.m
+++
@@ -1,7 +0,0 @@
-function y = s(t)%athugum að þetta fall á að taka lággildi í neðstu stöðu og hágildi í efstu
-    l = 1;
-    a = 0.2;
-    omega = 6;%þessi breytha verður að vera háð t þannig að  
-    phi = pi/2;
-    y = l+a*cos(omega*t+phi);
-end
