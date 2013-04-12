79547005be0e2ac9515b750acfe619b0600fe32b
Matthias Pall Gissurarson <mpg@mpg.is>
2013-04-12 15:12:05 +0000

    5

--- H3/hr5.m
+++ H3/hr5.m
@@ -1,3 +1,3 @@
 %Forritsbutur sem byr til myndbandid af Lorentz attractor kerfi
 %Gaeti thurft ad minnka resid, en thetta gefur allavegana frekar smooth feril svona
-lorenzAnim(20,25,2^11,[[10;28;8/3],[10;65;12],[10;99.96;8/3],[10;0.5;8/3]], [[5,5,5];[1,1,1];[5,5,5];[5,5,5]], ['or', 'og', 'ob','oy'], ['r','g','b','y']);
+lorenzAnim(30,32,2^11,[[10;28;8/3],[10;65;12],[10;14;8/3],[10;0.5;8/3]], [[5,5,5];[5,5,5];[5,5,5];[5,5,5]], ['or', 'og', 'ob','oy'], ['r','g','b','y']);
