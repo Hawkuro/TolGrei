01fcd3d514a2fee0c76000689e0d27fc2a63c605
Matthías Páll Gissurarson <mpg@mpg.is>
2013-04-12 13:49:49 +0000

    baetti vid fleiri eindum

--- H3/hr5.m
+++ H3/hr5.m
@@ -1,3 +1,3 @@
 %Forritsbutur sem byr til myndbandid af Lorentz attractor kerfi
 %Gaeti thurft ad minnka resid, en thetta gefur allavegana frekar smooth feril svona
-lorenzAnim(20,25,2^11,[[10;28;8/3],[10;65;12]], [[5,5,5];[20,20,20]], ['r', 'g'], ['ob','og']);
+lorenzAnim(20,25,2^11,[[10;28;8/3],[10;65;12],[10;99.96;8/3],[10;0.5;8/3]], [[5,5,5];[1,1,1];[5,5,5];[5,5,5]], ['r', 'g', 'b','y'], ['or','og','ob','oy']);
