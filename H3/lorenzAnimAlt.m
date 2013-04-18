edef2643317d31f67458ff7ea9fc2d7b260407ea
Matthías Páll Gissurarson <mpg@mpg.is>
2013-04-18 15:31:32 +0000

    Getur tekið inn snúnings hraða

---
+++ H3/lorenzAnimAlt.m
@@ -0,0 +1,52 @@
+function y = lorenzAnimAlt(skjal,xspeed,yspeed,lotur,n,res,cond,points,markercolors,plotcolors)
+%% lorenzAnim.m
+% Skipanaskra sem byr til hreyfimynd af lorenz attractor,
+% tekur inn hve lengi a ad keyra, hve margir rammar a sekundu,
+% hversu margfalt meira er renderad af gildum, upphafsgildi
+% einda a formi lista [[sigma1;r1;b1], [sigma2;r2;b2]] etc
+% upphafs stadsetningar Ã¾eirra a formi
+% [[x0,y0,z0];[x0_2,y0_2,z0_2];...] etc sama lengd og
+% upphafsskilirdin
+% og svo tvo vigra markercolors og plotcolors sem segja til um
+% litina a eindunm og ferlunum sem thaer skilja eftir sig.
+
+aviobj = avifile(skjal,'compression','None','fps',16); %#ok<REMFF1>
+% Segir til um nafn myndbandsins, thjoppun og fjolda ramma a sek.
+% Windows notendur aettu ad breyta 'None' i 'Indeo5' eda i einhvern annan
+% compression moguleika.
+% Mac, Linux, BSD og onnur styrikerfi thurfa ad thjappa myndbandid
+% handvirkt utan matlab, t.d med ffmpeg.
+fig=figure;
+
+s = size(cond);
+fjoldi = s(2);
+%Byr til follin og reiknar ut nalgunina a theim
+for i = 1:fjoldi
+    lorenzFunc = @(t,x) lorenzODE(t,x,cond(:,i));
+    Sol(i, : , :) = adams_pc5(lorenzFunc,0,points(i,:),lotur,res*lotur*n);
+end
+
+whitebg('black')
+for t = 0:1/n:lotur
+    curr = res*floor(t*n)+1;
+    % Teiknar inn eindirnar
+    for i = 1:fjoldi
+        Solv = Sol(i,:,1:curr);
+        plot3(Solv(1,1,curr),Solv(1,2,curr),Solv(1,3,curr), markercolors(i), 'MarkerSize', 6) 
+        hold on
+        x = Solv(1,1,:);
+        y = Solv(1,2,:);
+        z = Solv(1,3,:);
+        plot3(x(:),y(:),z(:),plotcolors(i)) 
+        hold on
+    end
+    view(30+xspeed*t,30+yspeed*t)
+    grid %minor
+    hold off
+    %% Hreyfimynd
+    F = getframe(fig); %Naer i nyjasta ramman
+    aviobj = addframe(aviobj,F); % Og skeytir thvi vid restina
+end
+%% Fragangur
+close(fig); %Lokar myndinni til ad ekki se haegt ad yfirskrifa hana
+aviobj = close(aviobj); %Lokar og byr til myndbandid
